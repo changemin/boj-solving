d2e8f34b9e951652e33c3046343b701623bd42ed
Changemin <bkm.change.min@gmail.com>
2021-04-13 09:55:40 +0900

    Solve 1002

---
+++ 1000번~1099번/1001번 - A-B/solve.cpp
@@ -0,0 +1,30 @@
+//
+// 2021-04-13
+//
+// Created By 변경민
+//
+// 1001번 : A-B
+// https://www.acmicpc.net/problem/1001
+//
+// * 문제
+//
+// 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+//
+// * 입력
+//
+// 3 2
+//
+// * 출력
+//
+// 1
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    cin>>a>>b;
+    cout<<a-b;    
+    return 0;
+}
