99d3ddf06e6bcaff278f24f83e895b89d1f05853
Changemin <bkm.change.min@gmail.com>
2021-04-13 09:48:09 +0900

    first commit

---
+++ 1000번~1099번/1000번 - A+B/solve.cpp
@@ -0,0 +1,30 @@
+//
+// 2021-04-12
+//
+// Created By 변경민
+//
+// 1000번 : A+B
+// https://www.acmicpc.net/problem/1000
+//
+// * 문제
+//
+// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+//
+// * 입력
+//
+// 1 2
+//
+// * 출력
+//
+// 3
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    cin>>a>>b;
+    cout<<a+b;    
+    return 0;
+}
